699cd8e228f95e9a69bee28e04490ebf7a1f1558
Baylie Warrick <bayliewarrick2019@gmail.com>
2021-08-18 15:46:02 -0500

    configured test to also expect an array

--- tests/post.js
+++ tests/post.js
@@ -6,11 +6,12 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('/GET Posts', () => {
-    it('it should get all of the posts.', (done) => {
+    it('Route should send a 200 response containing an array of posts.', (done) => {
         chai.request(`http://localhost:1234`)
         .get('/posts')
         .end(function (err, res) {
             res.should.have.status(200);
+            res.body.should.be.an('array');
             done();
         })
     })
